7a2fd0755fb10bc8b88197b266b7dd89c0ad8b7c
saurabh4060 <124861320+saurabh4060@users.noreply.github.com>
2025-01-07 14:12:46 +0530

    Update dash.py

--- dash.py
+++ dash.py
@@ -15,7 +15,7 @@ if f1 is not None:
     st.write(filename)
     df = pd.read_csv(filename,encoding="ISO-8859-1")
 else:
-    os.chdir("C:/Users/ROCKSTAR/Desktop")
+    os.chdir("C:/Users/ROCKSTAR/Desktop/example/Superstore.csv")
     df= pd.read_csv("C:/Users/ROCKSTAR/Desktop/example/Superstore.csv")
 
 
